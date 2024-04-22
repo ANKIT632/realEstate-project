bef620d01a6804967c2a6387d022be1253a878bb
ankit gupta <ankitgupta17878@gmail.com>
2024-04-22 11:35:45 +0530

    -

--- client/src/components/topNavBar.component.jsx
+++ client/src/components/topNavBar.component.jsx
@@ -1,11 +1,12 @@
-
 function TopNavBar() {
   return (
-    <div className="w-full h-10 bg-sky-900  sticky top-0">
-     
-     <h1 className="text-center text-white p-[3px] font-serif font-extrabold text-[22px]">Estate-Ease</h1>
+    <div className="w-full sticky h-10 bg-sky-900 ">
+      <h1 className="text-white p-[3px] font-serif font-extrabold text-[22px] ml-2">Estate-Ease</h1>
+
+   
+        <input type="text" placeholder="find here" className=" h-7 pl-3 rounded-lg ml-2.5 mt-1 bg-sky-50 border border-gray-500 md: md:absolute top-1 md:right-10 xs:w-full " /> 
     </div>
   )
 }
 
-export default TopNavBar
+export default TopNavBar;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,8 +11,6 @@ export default {
         'sm': '540px',
         'md': '768px',
         'lg': '1024px',
-        'xl': '1280px',
-        '2xl': '1536px',
       },
     },
   },
