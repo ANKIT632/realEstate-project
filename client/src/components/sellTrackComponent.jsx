89c9d3bc4acd8baae8ddee2cd8288f8e8c5c42ab
ankit gupta <ankitgupta17878@gmail.com>
2024-06-05 00:57:02 +0530

    -

--- client/src/components/sellTrackComponent.jsx
+++ client/src/components/sellTrackComponent.jsx
@@ -1,20 +1,20 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-undef */
 
-import {  useState,useContext } from 'react'
+import { useState, useContext } from 'react'
 import { commonStyle } from '../style'
 import { useLocation } from 'react-router-dom';
 import UserDataContext from "../context/userContext";
 
 
-function SellTrackComponent({ data,date }) {
+function SellTrackComponent({ data, date }) {
 
   // get all visitor of property
   const [visitorData, setVisitorData] = useState({});
-  const [isVisitShow,setIsVisitShow]=useState(true);
+  const [isVisitShow, setIsVisitShow] = useState(true);
   const { accessToken } = useContext(UserDataContext);
- 
-  const location=useLocation();
+
+  const location = useLocation();
 
   const getVisitorData = async (propertyId) => {
     try {
@@ -23,15 +23,16 @@ function SellTrackComponent({ data,date }) {
         headers: {
           'Content-Type': 'application/json',
           'authorization': `Bearer ${accessToken}`
-          
+
         }
       });
 
 
       const data = await response.json();
-      setIsVisitShow((pre)=>!pre);
+      console.log(data)
+      setIsVisitShow((pre) => !pre);
       setVisitorData(data);
-  
+
 
     }
 
@@ -42,14 +43,14 @@ function SellTrackComponent({ data,date }) {
 
 
   return (
- <div className='h-fit bg-white mb-3 py-3 w-[95%]  px-2 rounded-xl  shadow-md hover:shadow-lg '>
+    <div className='h-fit bg-white mb-3 py-3 w-[95%]  px-2 rounded-xl  shadow-md hover:shadow-lg '>
 
 
       <div className='flex max-xs:flex-col'>
-       
-          <div className='w-[200px] max-sm:w-[180px] max-xs:w-[150px]'>
-            <img src={data?.imagesUrl[0]} alt='img' className='rounded-2xl w-full h-32 object-cover bg-gray-400 max-sm:h-28 max-xs:h-20' />
-          </div>
+
+        <div className='w-[200px] max-sm:w-[180px] max-xs:w-[150px]'>
+          <img src={data?.imagesUrl[0]} alt='img' className='rounded-2xl w-full h-32 object-cover bg-gray-400 max-sm:h-28 max-xs:h-20' />
+        </div>
 
 
         <div className='pl-2 pt-1 md:pl-6'>
@@ -60,40 +61,42 @@ function SellTrackComponent({ data,date }) {
 
           <h3 className={"  text-xs  font-bold text-black  "}>Description : <strong className='text-blue-600 font-medium'> {data.description}</strong></h3>
 
-          <h3 className={"  text-xs  font-bold text-black  "}>Status : <strong className='text-blue-600 font-medium'> {data?.isSold?"Sold Out":"Not Sold"}</strong></h3>
+          <h3 className={"  text-xs  font-bold text-black  "}>Status : <strong className='text-blue-600 font-medium'> {data?.isSold ? "Sold Out" : "Not Sold"}</strong></h3>
 
-          
-         {location.pathname==='/buyTrack' && <h3 className={`text-xs  font-bold text-black  `}>Negotiable Price : <strong className='text-blue-600 font-medium'> {data. nagotiate?"Yes":"No"}</strong></h3>
-         }
+
+          {location.pathname === '/buyTrack' && <h3 className={`text-xs  font-bold text-black  `}>Negotiable Price : <strong className='text-blue-600 font-medium'> {data.nagotiate ? "Yes" : "No"}</strong></h3>
+          }
           {/* <h3 className={"  text-xs   font-bold text-black"}>Sold : <strong className='text-blue-600 font-medium'> {data.isSold ? 'close for Buy' : 'open for Buy'}</strong></h3> */}
 
 
-          {isVisitShow && <button className={commonStyle.btn + ` relative text-xs left-0 mt-1 ${ location.pathname==='/buyTrack' ? ' hidden' :" "}`} onClick={() => getVisitorData(data._id)} >Show visitors</button>}
+          {isVisitShow && <button className={commonStyle.btn + ` relative text-xs left-0 mt-1 ${location.pathname === '/buyTrack' ? ' hidden' : " "}`} onClick={() => getVisitorData(data._id)} >Show visitors</button>}
 
-          {location.pathname==='/buyTrack' && <h3 className={`text-xs  font-bold text-black  `}>Visit At : <strong className='text-blue-600 font-medium'> {date}</strong></h3>
-         }
+          {location.pathname === '/buyTrack' && <h3 className={`text-xs  font-bold text-black  `}>Visit At : <strong className='text-blue-600 font-medium'> {date}</strong></h3>
+          }
 
         </div>
       </div>
 
-      {!isVisitShow && <div className={`h-[100px] w-full bg-gray-200 mt-2 flex space-x-1 items-center rounded-md`}>
+      {!isVisitShow && !visitorData?.message && <div className={`h-[100px] w-full bg-gray-200 mt-2 flex space-x-1 items-center rounded-md`}>
         {
 
           visitorData?.visitors?.visitors?.map((data, idx) => {
-         
+
 
             return (
-                
+
               <div key={idx} className='m-1 flex flex-col items-center'>
                 <img src={data?.visitorDetails?.profile_url} alt='img' className='h-[40px] w-[40px] rounded-[50%] bg-gray-300 ' />
                 <strong className='text-xs text-blue-600 font-medium'> {data.visitorDetails.username}</strong>
-                <h4 className='text-xs  font-bold text-black'>{data?.visitedAt.slice(0,10)}</h4>
+                <h4 className='text-xs  font-bold text-black'>{data?.visitedAt.slice(0, 10)}</h4>
               </div>
             )
           })
         }
       </div>
       }
+
+      {visitorData?.message && !isVisitShow && <h3 className='text-xs text-red-600 font-bold mt-2'>{visitorData.message}</h3>}
 
     </div>
   )
