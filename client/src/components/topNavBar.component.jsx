0ca4c339b788b9c524dd704bdb31d08695d0e4cb
Ankit Gupta <ankitgupta17878@gmail.com>
2025-12-27 20:20:11 +0530

    fixed nav

--- client/src/components/topNavBar.component.jsx
+++ client/src/components/topNavBar.component.jsx
@@ -4,8 +4,7 @@ import { Link, useNavigate, useLocation } from "react-router-dom";
 import { commonStyle } from '../style';
 import { BiAlignRight } from "react-icons/bi";
 import UserDataContext from '../context/userContext';
-import { FaArrowLeft } from "react-icons/fa";
-import { IoSearchCircleSharp } from "react-icons/io5";
+import { FaArrowLeft,FaSearch } from "react-icons/fa";
 import  {logoutUser}  from "../utils/auth";
 
 function TopNavBar() {
@@ -95,7 +94,7 @@ function TopNavBar() {
     <div className="w-full sticky top-0 z-50">
       <nav className={`w-full   h-14 bg-white   border-b  overflow-hidden ${searchBoxVisibility ? 'max-md:hidden ' : ''}}`}>
         
-        <h4 className="text-blue-600 w-fit relative top-2.5 left-2 font-poppins font-extrabold text-[22px]  cursor-pointer active:text-blue-800" onClick={() => navigate('/')}>EcoEstate</h4>
+        <h4 className="text-blue-800 w-fit relative top-2.5 left-2 font-poppins font-extrabold text-[22px]  cursor-pointer active:text-blue-800" onClick={() => navigate('/')}>EcoEstate</h4>
 
 
         {userData?.role === 'Seller' && <button className=" absolute top-1 -my-3 font-bold  right-20  text-[40px] max-md:hidden active:text-blue-500" onClick={handleAddProperty}>+</button>
@@ -111,7 +110,7 @@ function TopNavBar() {
                 <BiAlignRight className={`h-5 w-5 ${showUserMenue ? " text-blue-500" : "text-white"}`} />
               </div>
 
-              : <button className={commonStyle.btn + "fixed top-4 px-2"} onClick={authHandller}>Sign In</button>}
+              : <button className={commonStyle.btn + "fixed top-[13px] px-2"} onClick={authHandller}>Sign In</button>}
         </span>
 
         {
@@ -128,7 +127,7 @@ function TopNavBar() {
 
                 {userData?.role === 'Buyer' && <Link to="/buyTrack" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" onClick={HandleToggleMenueOnLink}>visit schedule</Link>}
 
-                {/* <Link to="/favourite" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" onClick={HandleToggleMenueOnLink} >favourite</Link> */}
+                <Link to="/favourite" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" onClick={HandleToggleMenueOnLink} >favourite</Link>
 
                 <Link to="/" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" onClick={logoutHandler} >Logout</Link>
               </div>
@@ -140,12 +139,12 @@ function TopNavBar() {
 
       {/* search div */}
 
-      <div className={`flex items-center max-md:bg-white  max-md:shadow-sm max-md:h-14 ${searchBoxVisibility ? '' : 'max-md:hidden '} md:right-[120px]  md:top-3 md:absolute text-2xl `}>
+      <div className={`flex items-center max-md:bg-white  max-md:shadow-sm max-md:h-14 ${searchBoxVisibility ? '' : 'max-md:hidden '} md:right-[80px]  md:top-3 md:absolute text-2xl `}>
         <FaArrowLeft className="md:hidden w-[6%] active:text-blue-500 cursor-pointer " onClick={() => setSearchBoxVisibility((pre) => !pre)} />
 
-        <input type="text" placeholder="Find by name,location,city" className={`text-xs   h-10 md:h-8 max-md:w-[95%]  pl-4  bg-sky-50 border border-gray-500    rounded-lg  outline-none    focus:border-blue-500  pr-8 md:w-[340px]  max-md:rounded-2xl`} value={localSearch} onChange={searchChangeHandler} onKeyDownCapture={searchHandler} />
+        <input type="text" placeholder="Find by name,location,city" className={`text-xs h-10 md:h-8 max-md:w-[95%]  pl-4  bg-sky-50 border border-gray-200  rounded-md  outline-none  focus:border-blue-200  pr-8 md:w-[300px]  max-md:rounded-2xl`} value={localSearch} onChange={searchChangeHandler} onKeyDownCapture={searchHandler} />
 
-        <IoSearchCircleSharp className="text-4xl active:text-blue-500 max-md:fixed  max-md:right-0  md:absolute  md:right-0  cursor-pointer md:text-3xl" onClick={handlerIconSearchQuery} />
+        <FaSearch className="active:text-blue-600 max-md:fixed right-1 rounded border p-1  md:absolute  cursor-pointer text-[25px] bg-white" onClick={handlerIconSearchQuery} />
       </div>
     </div>
   )
--- client/src/style.js
+++ client/src/style.js
@@ -1,7 +1,7 @@
 export const commonStyle={
-    btn:"font-medium text-[14px] text-white bg-blue-400 px-1 rounded-sm py-0.5 shadow-lg cursor-pointer fixed right-2 hover:bg-blue-500 active:bg-blue-600",
+    btn:"font-medium text-[14px] text-white bg-blue-500 px-1 rounded-sm py-1 shadow-sm cursor-pointer fixed right-2 hover:bg-blue-600 active:bg-blue-500",
 
-    authBtn:"px-2 py-1 rounded-[5px] bg-blue-400 text-white font-bold shadow-lg hover:bg-blue-500 active:bg-blue-600 cursor-pointer w-[40%] md:w-[20%]",
+    authBtn:"px-2 py-1 rounded-[5px] bg-blue-400 text-white font-bold shadow-sm hover:bg-blue-600 active:bg-blue-500 cursor-pointer w-[40%] md:w-[20%]",
 
     title:"text-center font-mono font-bold text-[20px]  mb-2 text-blue-500 max-xs:text-[15px]",
 
